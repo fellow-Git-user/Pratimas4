b196c66a1cecd181422741699b7c0f502bc20426
fellow-Git-user <martynas.nadeika@gmail.com>
2024-12-09 19:22:20 +0200

    game conquered

--- JS/script.js
+++ JS/script.js
@@ -14,35 +14,94 @@
 //     }
 // }
 
-let age = prompt('Write in the age');
+// let age = prompt('Write in the age');
 
-if (age < 0){
-    console.log( "Entered number is too low" );
-    } else if (age < 6){
-        console.log( "Does't go to school" );
-    } else if ( age == 6){
-        console.log( "Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas." )
-    } else if (age == 10){
-        console.log( "Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas." )
-    } else if (age >= 7 && age <= 10){
-        console.log( "Goes to elementary school" );
-    } else if (age == 14){
-        console.log( "Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas." )
-    } else if (age >= 11 && age <= 14){
-        console.log( "Goes to primary school" )
-    } else if (age == 18){
-        console.log( "Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs." )
-    } else if (age >= 15 && age <= 18){
-        console.log( "Goes to gymnasium" )
-    } else if ( age >= 19 && age <= 120){
-        console.log( "School finished" )
-    } else if (age > 120){
-        console.log( "Entered number is too big" )
-    } else if (age !== Number){
-        console.log( "Netinkamai nurodytas amžius, amžius privalo būti skaičius." )
-    }
+// if (age < 0){
+//     console.log( "Entered number is too low" );
+//     } else if (age < 6){
+//         console.log( "Does't go to school" );
+//     } else if ( age == 6){
+//         console.log( "Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas." )
+//     } else if (age == 10){
+//         console.log( "Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas." )
+//     } else if (age >= 7 && age <= 10){
+//         console.log( "Goes to elementary school" );
+//     } else if (age == 14){
+//         console.log( "Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas." )
+//     } else if (age >= 11 && age <= 14){
+//         console.log( "Goes to primary school" )
+//     } else if (age == 18){
+//         console.log( "Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs." )
+//     } else if (age >= 15 && age <= 18){
+//         console.log( "Goes to gymnasium" )
+//     } else if ( age >= 19 && age <= 120){
+//         console.log( "School finished" )
+//     } else if (age > 120){
+//         console.log( "Entered number is too big" )
+//     } else if (age !== Number){
+//         console.log( "Netinkamai nurodytas amžius, amžius privalo būti skaičius." )
+//     }
 
 
+// Kuri narsykle populiaresne?
+
+// let questionFirst = Boolean('chrome') === true;
+// let questionSecond = Boolean('edge') === false;
+
+// // kam priklauso chrome?
+
+// let questionThird = Boolean('google') === true;
+// let questionFourth = Boolean('microsoft') === false;
+
+// let answerFirst = prompt('Kuri narsykle populiaresne?');
+// let answerSecond = prompt('kam priklauso chrome?');
+
+// if (answerFirst == 'chrome' && answerSecond == 'google'){
+//     console.log( 'Patekai i kita lygi: abu atsakymai teisingi' );
+// } else if (answerFirst != 'chrome' && answerSecond == 'google'){
+//     console.log( ' Patekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.' );
+// } else if (answerFirst == 'chrome' && answerSecond != 'google'){
+//     console.log( 'Patekai i kita lygi: antras atsakymas buvo neteisingas, o pirmas buvo teisingas.' );
+// } else if (answerFirst != 'chrome' && answerSecond != 'google'){
+//     console.log( 'Nepatekai i kita lygi: abu atsakymai buvo neteisingi.' );
+// } 
+
+// kuri narsykle populiaresne?
+// let questionFirst = Boolean('chrome') === true;
+// let questionSecond = Boolean('edge') === false;
+
+// // kam priklauso chrome?
+
+// let questionThird = Boolean('google') === true;
+// let questionFourth = Boolean('microsoft') === false;
+
+// // apple laptopo vardas
+
+// let questionFifth = Boolean('macbook') === true;
+// let questionSixth = Boolean('surface') === false; 
+
+// let answerFirst = prompt('Kuri narsykle populiaresne?');
+// let answerSecond = prompt('kam priklauso chrome?');
+// let answerThird = prompt('apple laptopo vardas');
+
+
+// if (answerFirst == 'chrome' && answerSecond == 'google' && answerThird == 'macbook'){
+//     console.log( 'patenkama i kita lygi:    1 1 1' );
+// } else if (answerFirst == 'chrome' && answerSecond == 'google' && answerThird !='macbook'){
+//     console.log( ' patenkama i kita lygi:    1 1 0' );
+// } else if (answerFirst == 'chrome' && answerSecond != 'google' && answerThird == 'macbook' ){
+//     console.log( 'patenkama i kita lygi:    1 0 1' );
+// } else if (answerFirst != 'chrome' && answerSecond == 'google' && answerThird == 'macbook' ){
+//     console.log( 'patenkama i kita lygi:    0 1 1' );
+// } else if (answerFirst == 'chrome' && answerSecond != 'google' && answerThird != 'macbook'){
+//     console.log( 'nepatenkama i kita lygi:  1 0 0' );
+// } else if (answerFirst != 'chrome' && answerSecond == 'google' && answerThird !='macbook'){
+//     console.log( ' nepatenkama i kita lygi:  0 1 0' );
+// } else if (answerFirst != 'chrome' && answerSecond != 'google' && answerThird == 'macbook' ){
+//     console.log( 'nepatenkama i kita lygi:  0 0 1' );
+// } else if (answerFirst != 'chrome' && answerSecond != 'google' && answerThird != 'macbook' ){
+//     console.log( 'nepatenkama i kita lygi:  0 0 0chrome' );
+// } 
 
 
  
